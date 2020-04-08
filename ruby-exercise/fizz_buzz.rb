33ebc2bbb27c247751ee9d8b7b2e06644ab8a18b
morimotohirotomo <minamoyori19@outlook.jp>
2020-04-08 13:44:13 +0900

    fizzbuzz

--- ruby-exercise/fizz_buzz.rb
+++ ruby-exercise/fizz_buzz.rb
@@ -1,19 +1,35 @@
-def fizz_buzz(input)
-  if input % 15 == 0
-    puts 'FizzBuzz'
-  elsif input % 3 == 0
-    puts 'Fizz'
-  elsif input % 5 == 0
-    puts 'Buzz'
+# def fizz_buzz(input)
+#   if input % 15 == 0
+#     puts 'FizzBuzz'
+#   elsif input % 3 == 0
+#     puts 'Fizz'
+#   elsif input % 5 == 0
+#     puts 'Buzz'
+#   else
+#     puts input.to_s
+#   end
+# end
+
+# puts "1 以上の数字を入力してください"
+
+# input = gets.to_i
+
+# puts '結果は、、、'
+
+# puts fizz_buzz(input)
+
+def fizzbuzz(n)
+  if n % 15 == 0
+  	puts "fizzbuzz"
+  elsif n % 3 == 0
+  	puts "fizz"
+  elsif n % 5 == 0
+  	puts "buzz"
   else
-    puts input.to_s
+  	puts n
   end
 end
 
-puts "1 以上の数字を入力してください"
-
-input = gets.to_i
-
-puts '結果は、、、'
-
-puts fizz_buzz(input)
+for n in 1..10
+  puts fizzbuzz(n)
+end
